1db5d568d79cc77d089a3225ef60593157c0c591
kaztro <00229017@uca.edu.sv>
2019-03-27 10:38:35 -0600

    Sigo en kotlin posible solucion a NetworkUtils//Sin probar

--- app/src/main/java/com/example/pokedex/utilities/NetworkUtils.kt
+++ app/src/main/java/com/example/pokedex/utilities/NetworkUtils.kt
@@ -3,15 +3,15 @@ package com.example.pokedex.utilities
 import android.net.Uri
 import android.util.Log
 import java.io.IOException
-import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.MalformedURLException
 import java.net.URL
+import java.util.*
 
 class NetworkUtils {
     val POKEMON_API_BASE_URL: String = "https://pokeapi.co/api/v2"
     val POKEMON_INFO: String = "pokemon"
-    var TAG: String? = NetworkUtils::class.simpleName
+    var TAG: String? = NetworkUtils::class.java.simpleName
 
     fun buildURL(pokeID: String): URL {
         var buildUri: Uri = Uri.parse(POKEMON_API_BASE_URL).buildUpon().appendPath(POKEMON_INFO).appendPath(pokeID).build()
@@ -24,10 +24,59 @@ class NetworkUtils {
     }
 
     @Throws(IOException::class)
-    fun getResponseFromHttpUrl(url: URL): String {
+    fun getResponseFromHttpUrl(url: URL): String? {
+        val urlConnection: HttpURLConnection = url.openConnection() as HttpURLConnection
+
+        try{
+            urlConnection.inputStream.bufferedReader().use { reader ->
+                val scanner = Scanner(reader)
+
+                scanner.useDelimiter("\\A")
+
+                var hasInput: Boolean = scanner.hasNext()
+
+                return if(hasInput) {
+                    scanner.next()
+                } else {
+                    null
+                }
+            }
+        } finally {
+            urlConnection.disconnect()
+        }
+
+
+        /*
+        try{
+            var n: InputStream = urlConnection.inputStream.bufferedReader().readText()
+
+            val scanner = Scanner(n)
+
+            scanner.useDelimiter("\\A")
+
+            var hasInput: Boolean = scanner.hasNext()
+
+            return if(hasInput) {
+                scanner.next()
+            } else {
+                null
+            }
+        } finally {
+            urlConnection.disconnect()
+        }
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+        with(url.openConnection() as HttpURLConnection) {
+            requestMethod = "GET"
+
+            println("\nSent 'GET' request to URL : $url; Response Code : $responseCode")
+
+
+
+        }
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
         var urlConnection: HttpURLConnection = URL(HttpURLConnection).openConnection() as HttpURLConnection
         try {
             var _in: InputStream = urlConnection.inputStream.bufferedReader().readText()
-        }
+        }*/
     }
 }
